43511e0d182e2c956d6144186d889ed3f17f6806
Samuele <46517478+SamueleLonghin@users.noreply.github.com>
2023-06-06 15:59:35 +0200

    Pulizia

--- app/src/main/java/it/samuelelonghin/safelauncher/support/Variables.kt
+++ app/src/main/java/it/samuelelonghin/safelauncher/support/Variables.kt
@@ -1,29 +1,18 @@
 package it.samuelelonghin.safelauncher.support
 
 import android.app.Notification
-import android.content.BroadcastReceiver
 import android.content.Intent
 import android.content.SharedPreferences
-import android.graphics.Bitmap
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import it.samuelelonghin.safelauncher.list.apps.AppInfo
-import it.samuelelonghin.safelauncher.home.contacts.ContactInfo
-import it.samuelelonghin.safelauncher.home.contacts.ContactInfoPlaceholder
-import it.samuelelonghin.safelauncher.home.widgets.WidgetInfo
 import it.samuelelonghin.safelauncher.home.widgets.WidgetSerial
+import it.samuelelonghin.safelauncher.list.apps.AppInfo
 
 lateinit var launcherPreferences: SharedPreferences
 
 /* Objects used by multiple activities */
 val appsList: MutableList<AppInfo> = ArrayList()
-val contactsList: MutableMap<String, ContactInfoPlaceholder> = mutableMapOf()
+//val contactsList: MutableMap<String, ContactInfoPlaceholder> = mutableMapOf()
 var widgetsList: MutableList<WidgetSerial> = mutableListOf()
-
-var background: Bitmap? = null
-
-var dominantColor = 0
-var vibrantColor = 0
 
 var intendedSettingsPause = false // know when to close
 
